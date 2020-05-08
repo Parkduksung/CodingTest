b8781d44cb40da0abc827f9102b274d9fbdaefc7
Parkduksung <duksung1234@naver.com>
2020-05-08 14:25:05 +0900

    Lv1 2016문제

---
+++ app/src/main/java/com/work/codingtest/Lv1.kt
@@ -0,0 +1,70 @@
+package com.work.codingtest
+
+import java.util.*
+
+object Lv1 {
+
+    //이문제 맞추고 풀이들 보니까 import java.util.* 안해서 풀었는데 나중에 그렇게도 풀어볼것.
+    fun _2016(month: Int, day: Int): String {
+
+        val calendar = Calendar.getInstance()
+        calendar.set(2016, month - 1, day)
+
+        return when (calendar.get(Calendar.DAY_OF_WEEK)) {
+            Calendar.MONDAY -> {
+                "MON"
+            }
+            Calendar.TUESDAY -> {
+                "TUE"
+            }
+            Calendar.WEDNESDAY -> {
+                "WED"
+            }
+            Calendar.THURSDAY -> {
+                "THU"
+            }
+            Calendar.FRIDAY -> {
+                "FRI"
+            }
+            Calendar.SATURDAY -> {
+                "SAT"
+            }
+            Calendar.SUNDAY -> {
+                "SUN"
+            }
+            else -> {
+                ""
+            }
+        }
+//        val calendar = Calendar.getInstance()
+//        calendar.set(2016, month - 1, day)
+//
+//        return when (SimpleDateFormat("EEEE").format(calendar.time)) {
+//            "Monday" -> {
+//                "MON"
+//            }
+//            "Tuesday" -> {
+//                "TUE"
+//            }
+//            "Wednesday" -> {
+//                "WED"
+//            }
+//            "Thursday" -> {
+//                "THU"
+//            }
+//            "Friday" -> {
+//                "FRI"
+//            }
+//            "Saturday" -> {
+//                "SAT"
+//            }
+//            "Sunday" -> {
+//                "SUN"
+//            }
+//            else -> {
+//                ""
+//            }
+//        }
+    }
+
+}
