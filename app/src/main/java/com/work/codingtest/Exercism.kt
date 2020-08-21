ddf4179dd5eaa7b1d806e1e7ffa52635af4d5afd
Parkduksung <duksung1234@naver.com>
2020-08-21 20:54:34 +0900

    excecism_easy_rainDrop

--- app/src/main/java/com/work/codingtest/Exercism.kt
+++ app/src/main/java/com/work/codingtest/Exercism.kt
@@ -10,4 +10,26 @@ object Exercism {
 
 
 
+    fun convert(n: Int): String {
+        val getMultiplicationOfThree = when {
+            n % 3 == 0 -> "Pling"
+            else -> ""
+        }
+        val getMultiplicationOfFive = when {
+            n % 5 == 0 -> "Plang"
+            else -> ""
+        }
+        val getMultiplicationOfSeven = when {
+            n % 7 == 0 -> "Plong"
+            else -> ""
+        }
+        val toSum =
+            getMultiplicationOfThree + getMultiplicationOfFive + getMultiplicationOfSeven
+        if (toSum.isEmpty())
+            return "$n"
+        return toSum
+
+        // p.s  buildString 으로도 해보는걸 추천.
+
+    }
 }
